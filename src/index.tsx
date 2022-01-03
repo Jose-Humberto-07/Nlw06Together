e8d6a059d80614c1feead0d3f0974693370f157d
José Humberto <jhpnascimento96@gmail.com>
2022-01-03 17:07:38 -0300

    page Home and NewRoom

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import './services/firebase';
+
+import "./styles/global.scss";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
